a837e73223ffe67bfcf820b275231f687b3d0fb3
Alvzan <95696516+Alvzan@users.noreply.github.com>
2022-10-27 10:29:45 +0700

    Commit Lagu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const tanah = document.querySelectorAll('.tanah');
 const tikus = document.querySelectorAll('.tikus');
 const papanSkor = document.querySelector('.papan-skor');
-const pop = document.querySelector('#pop');
 
 let tanahSebelumnya;
 let selesai;
@@ -47,7 +46,6 @@ function mulai() {
 function pukul() {
   skor++;
   this.parentNode.classList.remove('muncul');
-  pop.play();
   papanSkor.textContent = skor;
 }
 
